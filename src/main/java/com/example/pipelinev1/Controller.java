fc1abe7e0be7c9cb4f0b3daea853716288f282f7
Shaundator <rash0081@kea.stud.dk>
2022-08-23 09:51:29 +0200

    v0

---
+++ src/main/java/com/example/pipelinev1/Controller.java
@@ -0,0 +1,11 @@
+package com.example.pipelinev1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
---
+++ src/test/java/com/example/pipelinev1/PipelineV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pipelinev1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
